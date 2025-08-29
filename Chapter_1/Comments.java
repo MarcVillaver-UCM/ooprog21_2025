af644733346d2ecf0a6c93cfbc173029d382167f
MarcVillaver-UCM <mlvillaver07@gmail.com>
2025-08-29 22:59:21 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,9 @@
+public class Comments {
+    // This is a line comment
+
+    /* This is a block comment */
+
+    /**
+     * This is a Javadoc comment
+     */
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Tensionado nagulat din ako,");
+        System.out.println("Nung malaman na hindi lang pala ako");
+        System.out.println("Yung nanghinayang nung nag away tayo nun");
+        System.out.println("At natuluyan sa iyakan at tampo");
+    }
+}
