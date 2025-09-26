0d2fa93c8196ce8c142ecf66730a3261e15478b0
MarcVillaver-UCM <mlvillaver07@gmail.com>
2025-09-26 09:30:06 +0800

    Add files via upload

---
+++ Chapter_4/DebugTrip.java
@@ -0,0 +1,39 @@
+// A Trip has three constructors
+// When one argument is passed,
+// it is the trip destination
+// When two arguments are passed,
+// they are the destination and departure cities
+// When three arguments are passed,
+// the last one is the mode of transportation for the trip
+public class DebugTrip{
+   private String destination;
+   private String departure;
+   private String mode;
+   private static final String DEFAULT_CITY = "Atlanta";
+   private static final String DEFAULT_MODE = "car";
+
+   public DebugTrip(String destination){
+      this(destination, DEFAULT_CITY, DEFAULT_MODE); 
+   }
+   public DebugTrip(String destination, String departure){
+      this(destination, departure, DEFAULT_MODE);  //tried to call two constructors at first. Fixed: Call the 3 argument constructor and use default mode
+   }  
+   
+   public DebugTrip(String destination, String departure, String mode){
+      this.destination = destination;
+      this.departure = departure;
+      this.mode = mode;
+   }
+   
+   public String getDestination(){
+      return destination;
+   }
+   
+   public String getDepartureCity(){
+      return departure;
+   }
+   
+   public String getMode(){
+      return mode;
+   }
+}
