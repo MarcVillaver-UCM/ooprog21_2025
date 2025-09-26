e9a547ea6509be63d5e8fb5fa6c80d99d915fa1f
MarcVillaver-UCM <mlvillaver07@gmail.com>
2025-09-26 10:25:31 +0800

    Update DebugFour1.java

--- Chapter_4/DebugFour1.java
+++ Chapter_4/DebugFour1.java
@@ -1,18 +1,18 @@
 // This class uses a DebugCircle class
 // to instantiate a Circle object
 import java.util.Scanner;
-public class DebugFour1{
-   public static void main(String args[]){
-      Scanner input = new Scanner(System.in);
-      int radius;
-      Circle c = new Circle();
-      
-      System.out.print("Enter a radius for a circle >> ");
-      radius = input.nextInt();
-      
-      c = new DebugCircle(c);
-      System.out.println("The radius is " + c.getRad());
-      System.out.println("The diameter is " + c.getDiam());
-      System.out.println("The area is " + c.getArea());
-   }
+public class DebugFour1 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int radius;
+
+        System.out.print("Enter a radius for a circle >> ");
+        radius = input.nextInt();
+
+         DebugCircle c = new DebugCircle(radius); // fixed pass the radius to the constructor instead of passing a Circle object
+
+        System.out.println("The radius is " + c.getRadius());
+        System.out.println("The diameter is " + c.getDiameter());
+        System.out.println("The area is " + c.getArea());
+    }
 }
