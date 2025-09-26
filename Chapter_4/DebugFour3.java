a0389603a282874ed5132a67627eff99c8a09144
MarcVillaver-UCM <mlvillaver07@gmail.com>
2025-09-26 10:08:48 +0800

    Add files via upload

---
+++ Chapter_4/DebugFour3.java
@@ -0,0 +1,39 @@
+
+// This class uses a DebugBox class
+// to instantiate two Box objects
+import java.util.Scanner;
+public class DebugFour3{
+   public static void main(String args[]){
+      Scanner input = new Scanner(System.in);
+      int w, l, h;
+      
+      System.out.print("Enter width of box >> ");
+      w = input.nextInt(); //fixed nextInteger() to nextInt()
+      
+      System.out.print("Enter length of box >> ");
+      l = input.nextInt(); //fixed nextDouble() to nextInt() to match the int type
+      
+      System.out.print("Enter height of box >> ");
+      h = input.nextInt();
+      
+      DebugBox box1 = new DebugBox(); //wrong class    name it is DebugBox() instead of FixDebugBox()
+      DebugBox box2 = new DebugBox(w, l, h); //wrong method name it is DebugBox() instead of FixDebugBox() and fixed error from box1 to box2
+      
+      System.out.println("The dimensions of the first box are");
+      box1.showData();
+      
+      System.out.print(" The volume of the first box is ");
+      showVolume(box1);
+      
+      System.out.println("The dimensions of the second box are");
+      box2.showData();
+      
+      System.out.print(" The volume of the second box is ");
+      showVolume(box2); //fixed box2.showVolume() to call static method
+   }
+   
+   public static void showVolume(DebugBox aBox){
+      double vol = aBox.getVolume();
+      System.out.println(vol);
+   }
+}
