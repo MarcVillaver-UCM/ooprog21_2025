f3bd054d4567aa1baba38b0b68d9c3b398a5e0dd
MarcVillaver-UCM <mlvillaver07@gmail.com>
2025-11-17 22:13:13 +0800

    Add files via upload

---
+++ Chapter_7/DebugSeven1.java
@@ -0,0 +1,31 @@
+// Makes String comparisons
+import java.util.*;
+
+public class DebugSeven1 {
+    public static void main(String[] args) {
+
+        Scanner kb = new Scanner(System.in);
+        String name1, name2, name3;
+
+        System.out.println("Enter three names.");
+        System.out.println("They can be the same or different.");
+        System.out.print("First name >> ");
+        name1 = kb.nextLine(); // missing parentheses for method call.
+        System.out.print("Second name >> ");
+        name2 = kb.nextLine(); // missing parentheses for method call.
+        System.out.print("Third name >> ");
+        name3 = kb.nextLine(); // missing parentheses for method call.
+
+        compareNames(name1, name2);
+        compareNames(name1, name3);
+        compareNames(name2, name3);
+    }
+
+    public static void compareNames(String n1, String n2) { // wrong method name and missing type for n2.
+        System.out.print(n1 + " and " + n2);
+        if (n1.equals(n2)) // missing closing parenthesis before.
+            System.out.println(" are the same");
+        else
+            System.out.println(" are different");
+    }
+}
