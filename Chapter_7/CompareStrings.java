303346cf0a35a97a48a1277d10571a1027309214
MarcVillaver-UCM <mlvillaver07@gmail.com>
2025-11-26 21:13:10 +0800

    Add files via upload

---
+++ Chapter_7/CompareStrings.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String predefined = "Carmen";
+
+        System.out.print("Enter your name > ");
+        String input = scanner.nextLine();
+
+        if (input.equals(predefined)) {
+            System.out.println("Carmen equals " + input);
+        } else {
+            System.out.println("Carmen does not equal " + input);
+        }
+
+        scanner.close();
+    }
+}
